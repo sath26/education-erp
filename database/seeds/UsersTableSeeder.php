ba4dfa04112851688e180f8ae8765002f8d51cd1
Anderson Altissimo <ander.altissimo@gmail.com>
2018-01-06 17:43:17 -0200

    first Commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Created by PhpStorm.
+ * User: andersonaltissimo
+ * Date: 1/6/18
+ * Time: 13:28
+ */
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(\App\Models\User::class)->create([
+           'email' => 'admin@system.com'
+        ]);
+    }
+}
